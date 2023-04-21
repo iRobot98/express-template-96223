d950c5cc45e990f70681d9b048c81dc68672e2c3
iRobot98 <matendejudah@gmail.com>
2023-04-21 16:27:05 +0300

    adding port 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(require("./src"));
 app.use("/", express.static("views"));
